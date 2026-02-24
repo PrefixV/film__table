94fd88c2ec835f031709ee7ef88e44c6d5752dae
PrefixV <54990471+PrefixV@users.noreply.github.com>
2026-02-24 18:45:22 +0300

    Release

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-// СМЕНА ТЕМЫ
+// ======================
+// ТЕМА
+// ======================
+
 const themeToggle = document.querySelector('.theme-toggle');
 const body = document.body;
 const lightIcon = document.querySelector('.light-icon');
 const darkIcon = document.querySelector('.dark-icon');
 
 function setTheme(theme) {
-    if(theme === "dark") {
+    if (theme === "dark") {
         body.classList.add("dark__theme");
         body.classList.remove("light__theme");
         darkIcon.classList.add("icon-active");
@@ -18,284 +21,203 @@ function setTheme(theme) {
         lightIcon.classList.add("icon-active");
         themeToggle.checked = false;
     }
-
     localStorage.setItem("theme", JSON.stringify(theme));
 }
 
-function toggleTheme() {
-    const currentTheme = JSON.parse(localStorage.getItem("theme")) || [];
-    const newTheme = currentTheme == 'light' ? 'dark' : 'light';
+themeToggle.addEventListener("change", () => {
+    const currentTheme = JSON.parse(localStorage.getItem("theme")) || "light";
+    const newTheme = currentTheme === "light" ? "dark" : "light";
     setTheme(newTheme);
-}
+});
 
-themeToggle.addEventListener('change', toggleTheme);
+document.addEventListener("DOMContentLoaded", () => {
+    const savedTheme = JSON.parse(localStorage.getItem("theme")) || "light";
+    setTheme(savedTheme);
+});
 
-document.addEventListener('DOMContentLoaded', () => {
-    const saveTheme = JSON.parse(localStorage.getItem("theme")) || [];
-    setTheme(saveTheme);
-})
+// ======================
+// ДАННЫЕ
+// ======================
 
-//РЕНДЕР ОШИБОК
-
-function renderError(message) {
-    const errorWrapper = document.querySelector('.error__wrapper');
-    const errorMessage = document.querySelector('.error__message');
-    errorMessage.textContent = message;
-    errorWrapper.classList.add("error__wrapper__active");
-    setTimeout(() => {
-        errorWrapper.classList.remove("error__wrapper__active");
-        errorMessage.textContent = "";
-    }, 2000)
-
-}
-
-//РЕНДЕР ТАБЛИЦЫ
-
-function renderTable(table) {
-    tableBody.innerHTML = "";
-
-    table.forEach((film, index) => {
-        const filmItem = document.createElement("tr");
-        filmItem.classList.add("table__row");
-        filmItem.dataset.index =  Number(index);
-        filmItem.dataset.id = film.id;
-        
-        if(film.isDone == true) {
-            filmItem.classList.add("film__done")
-        } else {
-            filmItem.classList.remove("film__done")
-        }
-
-        if(film.season == "" && film.series == "") {
-            film.series = "-";
-            film.season = "-";
-        }
-
-        filmItem.innerHTML = `
-            <td class="film__name">
-                ${film.name}
-            </td>
-            <td class="film__type">
-                ${film.type}
-            </td>
-            <td class="film__season">
-                ${film.season}
-            </td>
-            <td class="film__series">
-                ${film.series}
-            </td>
-            <td class="film__add__date">
-                ${new Date(film.timeCreate).toLocaleDateString()}
-            </td>
-            <td>
-                <button class="film__done__button" data-index="${index}">
-                    <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#141414" viewBox="0 0 24 24" >
-                        <path d="M9 15.59 4.71 11.3 3.3 12.71l5 5c.2.2.45.29.71.29s.51-.1.71-.29l11-11-1.41-1.41L9.02 15.59Z"></path>
-                    </svg>
-                </button>
-                <button class="film__delete__button" data-index="${index}">
-                    <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#141414" viewBox="0 0 24 24" >
-                        <path d="m7.76 14.83-2.83 2.83 1.41 1.41 2.83-2.83 2.12-2.12.71-.71.71.71 1.41 1.42 3.54 3.53 1.41-1.41-3.53-3.54-1.42-1.41-.71-.71 5.66-5.66-1.41-1.41L12 10.59 6.34 4.93 4.93 6.34 10.59 12l-.71.71z"></path>
-                    </svg>
-                </button>
-                <button class="film__edit__button" data-index="${index}">
-                    <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#141414" viewBox="0 0 24 24" >
-                        <path d="M5 21h14c1.1 0 2-.9 2-2v-7h-2v7H5V5h7V3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2"></path><path d="M7 13v3c0 .55.45 1 1 1h3c.27 0 .52-.11.71-.29l9-9a.996.996 0 0 0 0-1.41l-3-3a.996.996 0 0 0-1.41 0l-9.01 8.99A1 1 0 0 0 7 13m10-7.59L18.59 7 17.5 8.09 15.91 6.5zm-8 8 5.5-5.5 1.59 1.59-5.5 5.5H9z"></path>
-                    </svg>
-                </button>
-        `
-        tableBody.appendChild(filmItem);
-    });
-}
-
-const addFilmButton = document.querySelector('.film__create__button');
-
-// ОТКРЫТИЕ МОДАЛЬНОГО ОКНА ДОБАВЛЕНИЯ
-
-const modalTitle = document.querySelector('.modal__title');
-const openModalButton = document.querySelector(".nav__add__film__button");
-const addFilmModalWrapper = document.querySelector(".add__film__modal__wrapper");
-const closeModalButton = document.querySelector(".close__modal__button");
-const editFilmModalButton = document.querySelector(".film__edit__modal__button");
-
-openModalButton.addEventListener('click', () => {
-    addFilmModalWrapper.classList.add("add__film__modal__wrapper__active");
-    addFilmModalWrapper.dataset.mode = "add";
-    toggleMode("add");
-    document.querySelector(".film__input__name").value = "";
-    document.querySelector(".film__select__type").value = "";
-    document.querySelector(".film__input__series").value = "";
-    document.querySelector(".film__input__season").value = "";
-})
-
-closeModalButton.addEventListener('click', () => {
-    addFilmModalWrapper.classList.remove("add__film__modal__wrapper__active");
-})
-
-const films = JSON.parse(localStorage.getItem("films")) || [];
+let films = JSON.parse(localStorage.getItem("films")) || [];
 let currentFilms = [...films];
+
 const tableBody = document.querySelector(".table__body");
 
-// ДОБАВЛЕНИЕ ФИЛЬМА
+// ======================
+// ПАГИНАЦИЯ
+// ======================
+
+let currentPage = 1;
+const maxItemsPerPage = 10;
+
+function getPaginatedData(data) {
+    const startIndex = (currentPage - 1) * maxItemsPerPage;
+    return data.slice(startIndex, startIndex + maxItemsPerPage);
+}
+
+function renderPagination(data) {
+    const currentPageWrapper = document.querySelector(".current__page__wrapper");
+    const prevBtn = document.querySelector(".prev__page__button");
+    const nextBtn = document.querySelector(".next__page__wrapper");
+
+    currentPageWrapper.innerHTML = "";
+
+    const totalPages = Math.ceil(data.length / maxItemsPerPage);
+
+    for (let i = 1; i <= totalPages; i++) {
+        const btn = document.createElement("button");
+        btn.textContent = i;
+        btn.classList.add("page__button");
+
+        if (i === currentPage) {
+            btn.classList.add("active__page");
+        }
+
+        btn.addEventListener("click", () => {
+            currentPage = i;
+            renderTable(currentFilms);
+        });
+
+        currentPageWrapper.appendChild(btn);
+    }
+
+    prevBtn.onclick = () => {
+        if (currentPage > 1) {
+            currentPage--;
+            renderTable(currentFilms);
+        }
+    };
+
+    nextBtn.onclick = () => {
+        if (currentPage < totalPages) {
+            currentPage++;
+            renderTable(currentFilms);
+        }
+    };
+}
+
+// ======================
+// РЕНДЕР ТАБЛИЦЫ
+// ======================
+
+function renderTable(data) {
+    tableBody.innerHTML = "";
+    const paginatedData = getPaginatedData(data);
+    const totalWatchMoviesWrapper = document.querySelector(".total__watch__movies");
+    const totalWatch = films.filter(el => el.isDone == true);
+    totalWatchMoviesWrapper.textContent = `Просмотренных: ${totalWatch.length}`;
+
+    paginatedData.forEach((film) => {
+        const tr = document.createElement("tr");
+        tr.classList.add("table__row");
+        tr.dataset.id = film.id;
+
+        if (film.isDone) {
+            tr.classList.add("film__done");
+        }
+
+        tr.innerHTML = `
+            <td>${film.name}</td>
+            <td>${film.type}</td>
+            <td>${film.season || "-"}</td>
+            <td>${film.series || "-"}</td>
+            <td>${new Date(film.timeCreate).toLocaleDateString()}</td>
+            <td>
+                <button class="film__done__button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M9 16.2L4.8 12l-1.4 1.4L9 19L21 7l-1.4-1.4z"/></svg></button>
+                <button class="film__delete__button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="currentColor" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/></svg></button>
+                <button class="film__edit__button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M3.995 17.207V19.5a.5.5 0 0 0 .5.5h2.298a.5.5 0 0 0 .353-.146l9.448-9.448l-3-3l-9.452 9.448a.5.5 0 0 0-.147.353m10.837-11.04l3 3l1.46-1.46a1 1 0 0 0 0-1.414l-1.585-1.586a1 1 0 0 0-1.414 0z"/></svg></button>
+            </td>
+        `;
+
+        tableBody.appendChild(tr);
+    });
+
+    renderPagination(data);
+}
+
+// ======================
+// ДОБАВЛЕНИЕ
+// ======================
+
+const addFilmButton = document.querySelector('.film__create__button');
+const openModalButton = document.querySelector(".nav__add__film__button");
+const modalWrapper = document.querySelector(".add__film__modal__wrapper");
+const closeModalButton = document.querySelector(".close__modal__button");
 
 const filmTypeSelect = document.querySelector('.film__select__type');
 const seriesInput = document.querySelector('.film__input__series');
 const seasonInput = document.querySelector('.film__input__season');
 
-filmTypeSelect.addEventListener('change', () => {
-  if (filmTypeSelect.value === 'Сериал') {
-    seriesInput.removeAttribute('disabled'); 
-    seasonInput.removeAttribute('disabled');
-  } else {
-    seriesInput.setAttribute('disabled', 'disabled'); 
-    seasonInput.setAttribute('disabled', 'disabled');
-  }
+openModalButton.addEventListener("click", () => {
+    modalWrapper.classList.add("add__film__modal__wrapper__active");
 });
 
-addFilmButton.addEventListener('click', () => {
-    const filmName = document.querySelector('.film__input__name').value.trim();
-    const filmType = filmTypeSelect.value;
-    const filmSeries = seriesInput.value.trim();
-    const filmSeason = seasonInput.value.trim();
-    
-    if(!filmName) {
-        renderError("Введите название!");
-        return;
-    }
-
-   const filmItem = {
-  id: Date.now() + Math.random().toString(36).substr(2, 5),
-  name: filmName,
-  type: filmType,
-  series: filmSeries,
-  season: filmSeason,
-  timeCreate: Date.now(),
-  isDone: false,
-};
-
-    films.push(filmItem);
-    localStorage.setItem('films', JSON.stringify(films));
-    renderTable(films);
-    addFilmModalWrapper.classList.remove('add__film__modal__wrapper__active');
+closeModalButton.addEventListener("click", () => {
+    modalWrapper.classList.remove("add__film__modal__wrapper__active");
 });
 
-// РЕДАКТИРОВАНИЕ ФИЛЬМА
-
-tableBody.addEventListener('click', (e) => {
-    const editButton = e.target.closest(".film__edit__button");
-    if(!editButton) return;
-    const filmRow = editButton.closest('.table__row');
-    const id = filmRow.dataset.id
-
-    const index = films.findIndex(f => f.id === id);
-    if(index === -1) return;
-
-    const currentFilm = films[index];
-
-    
-    addFilmModalWrapper.classList.add("add__film__modal__wrapper__active");
-    addFilmModalWrapper.dataset.mode = "edit";
-    toggleMode("edit");
-
-    document.querySelector(".film__input__name").value = currentFilm.name;
-    document.querySelector(".film__select__type").value = currentFilm.type;
-    document.querySelector(".film__input__series").value = currentFilm.series;
-    document.querySelector(".film__input__season").value = currentFilm.season;
-    
-    window.currentEditIndex = index;
-    
-})
-
-function saveEditFilms() {
-
-    let filmName = document.querySelector('.film__input__name').value.trim();
-    let filmSeries = seriesInput.value.trim();
-    let filmType = filmTypeSelect.value;
-    let filmSeason = seasonInput.value.trim();
-
-
-        films[window.currentEditIndex] = {
-        ...films[window.currentEditIndex],
-        name: filmName,
-        type: filmType,
-        series: filmSeries,
-        season: filmSeason,
-    }
-
-    localStorage.setItem("films", JSON.stringify(films));
-    renderTable(films);
-
-    addFilmModalWrapper.classList.remove('add__film__modal__wrapper__active');
-    window.currentEditIndex = undefined; 
-}
-
-editFilmModalButton.addEventListener('click', saveEditFilms);
-
-function toggleMode(mode) {
-    mode = addFilmModalWrapper.dataset.mode;
-    if(mode === "add") {
-        modalTitle.textContent = "Добавить фильм"
-        editFilmModalButton.classList.add('display__none');
-        editFilmModalButton.classList.remove('display__normal');
-        addFilmButton.classList.remove('display__none');
-        addFilmButton.classList.add('display__normal');
-
-    } else if(mode === "edit") {
-        editFilmModalButton.classList.remove('display__none');
-        editFilmModalButton.classList.add('display__normal');
-        addFilmButton.classList.add('display__none');
-        addFilmButton.classList.remove('display__normal');
+filmTypeSelect.addEventListener("change", () => {
+    if (filmTypeSelect.value === "Сериал") {
+        seriesInput.disabled = false;
+        seasonInput.disabled = false;
     } else {
-        renderError("Кнопка ненайдена!");
+        seriesInput.disabled = true;
+        seasonInput.disabled = true;
     }
-}
-
-
-// УДАЛЕНИЕ ФИЛЬМА
-
-
-tableBody.addEventListener('click', (e) => {
-  const deleteFilmButton = e.target.closest('.film__delete__button');
-  if (!deleteFilmButton) return;
-
-
-  const filmRow = deleteFilmButton.closest('.table__row');
-  const id = filmRow.dataset.id;
-
-  const index = films.findIndex(f => f.id === id);
-  if (index === -1) return;
-
-  films.splice(index, 1);
-  localStorage.setItem("films", JSON.stringify(films));
-  sortNSearch();
 });
 
-// СТАТУС ПРОСМОТРЕННО
+addFilmButton.addEventListener("click", () => {
+    const name = document.querySelector('.film__input__name').value.trim();
+    if (!name) return alert("Введите название");
 
-tableBody.addEventListener('click', (e) => {
-    const filmDoneButton = e.target.closest(".film__done__button");
-    const filmTableRow = e.target.closest(".table__row");
+    const newFilm = {
+        id: Date.now().toString(),
+        name,
+        type: filmTypeSelect.value,
+        series: seriesInput.value.trim(),
+        season: seasonInput.value.trim(),
+        timeCreate: Date.now(),
+        isDone: false,
+    };
 
-    if(!filmDoneButton) return;
-    const id = filmTableRow.dataset.id;
-    const index = films.findIndex(f => f.id === id);
-    console.log(index);
-    
-    if (index === -1) {
-        renderError('Фильм не найден по id:', id);
-        return;
-    }
-
-    films[index].isDone = !films[index].isDone;
-    films[index].timeCreate = Date.now();
+    films.push(newFilm);
+    localStorage.setItem("films", JSON.stringify(films));
 
     currentFilms = [...films];
+    currentPage = 1;
+    renderTable(currentFilms);
+
+    modalWrapper.classList.remove("add__film__modal__wrapper__active");
+});
+
+// ======================
+// УДАЛЕНИЕ / DONE / EDIT
+// ======================
+
+tableBody.addEventListener("click", (e) => {
+    const row = e.target.closest(".table__row");
+    if (!row) return;
+
+    const id = row.dataset.id;
+    const index = films.findIndex(f => f.id === id);
+    if (index === -1) return;
+
+    if (e.target.closest(".film__delete__button")) {
+        films.splice(index, 1);
+    }
+
+    if (e.target.closest(".film__done__button")) {
+        films[index].isDone = !films[index].isDone;
+    }
+
     localStorage.setItem("films", JSON.stringify(films));
     sortNSearch();
-    renderTable(currentFilms);
-})
+});
 
-// ФИЛЬТРАЦИЯ МАССИВА
+// ======================
+// СОРТИРОВКА И ПОИСК
+// ======================
 
 const selectFilter = document.querySelector('.sort__select');
 const searchInput = document.querySelector(".search__film");
@@ -304,44 +226,37 @@ function sortNSearch() {
     const type = selectFilter.value;
     let result = [...films];
 
-    if(type === "Просмотренные") {
-        result = result.filter(el => el.isDone === true);
-    } else {
-        switch(type) {
-            case "Тип":
-                result.sort((a,b) => {
-                    return b.type?.toLowerCase()?.localeCompare(a.type?.toLowerCase() || 0)
-                }) 
-                break
-            case "Название":
-                result.sort((a,b) => {
-                    return a.name?.toLowerCase()?.localeCompare(b.name?.toLowerCase() || 0)
-                })
-                break
-            case "Дата добавления":
-                result.sort((a,b) => {
-                    return new Date(a.timeCreate).getTime() - new Date(b.timeCreate).getTime()
-                })
-                break
-        }
+    if (type === "Просмотренные") {
+        result = result.filter(f => f.isDone);
     }
 
-    const query = document.querySelector(".search__film").value.trim().toLowerCase();
+    if (type === "Название") {
+        result.sort((a, b) => a.name.localeCompare(b.name));
+    }
+
+    if (type === "Дата добавления") {
+        result.sort((a, b) => b.timeCreate - a.timeCreate);
+    }
+
+    const query = searchInput.value.trim().toLowerCase();
 
     if (query) {
-    result = result.filter(film => 
-      film.name?.toLowerCase().includes(query) ||
-      film.type?.toLowerCase().includes(query)
-    );
-  }
+        result = result.filter(f =>
+            f.name.toLowerCase().includes(query) ||
+            f.type.toLowerCase().includes(query)
+        );
+    }
 
     currentFilms = result;
+    currentPage = 1;
     renderTable(currentFilms);
 }
 
-selectFilter.addEventListener('change', sortNSearch);
-searchInput.addEventListener('input', sortNSearch);
+selectFilter.addEventListener("change", sortNSearch);
+searchInput.addEventListener("input", sortNSearch);
 
-// ФИНАЛЬНЫЙ РЕНДЕР
+// ======================
+// СТАРТ
+// ======================
 
-renderTable(films);
+renderTable(currentFilms);
